7842a653e739963455adf14ccf7fe8ce2e1ed79f
Gabriel Wu <qqbbnease1004@126.com>
2018-10-26 22:00:16 +0800

    travis

--- deploy/now.sh
+++ deploy/now.sh
@@ -2,6 +2,6 @@
 
 export NOW_SUBDOMAIN=koa-api-server
 
-now --token $NOW_TOKEN
-now alias --token $NOW_TOKEN
+NOW_DEPLOY_ID=$( now --token $NOW_TOKEN )
+now alias $NOW_DEPLOY_ID --token $NOW_TOKEN
 now rm $NOW_SUBDOMAIN --safe --yes --token $NOW_TOKEN
--- tests/server/poems.test.ts
+++ tests/server/poems.test.ts
@@ -1,10 +1,12 @@
 import Debug from 'debug';
 import request from 'supertest';
 import uuid from 'uuid';
+import {CODE, MESSAGE} from '../../src/common/response';
 import knex from '../../src/server/db/connection';
 import server from '../../src/server/index';
 
 const debug = Debug('test:poems');
+
 beforeEach(() => {
   debug('Database to be migrated!');
   return knex.migrate
@@ -31,7 +33,7 @@ afterEach(() => {
 describe('routes: poems', () => {
   test('GET all poems via /api/v1/poems', async () => {
     const response = await request(server).get('/api/v1/poems');
-    expect(response.status).toEqual(200);
+    expect(response.status).toEqual(CODE.OK);
     expect(response.type).toEqual('application/json');
     expect(Object.keys(response.body.data[0]).sort()).toEqual(
       ['id', 'name', 'author', 'votes', 'created_at', 'updated_at'].sort(),
@@ -40,7 +42,7 @@ describe('routes: poems', () => {
 
   test('GET a poem via /api/v1/poems/:id', async () => {
     const response = await request(server).get('/api/v1/poems/1');
-    expect(response.status).toEqual(200);
+    expect(response.status).toEqual(CODE.OK);
     expect(response.type).toEqual('application/json');
     expect(Object.keys(response.body.data[0]).sort()).toEqual(
       ['id', 'name', 'author', 'votes', 'created_at', 'updated_at'].sort(),
@@ -49,7 +51,7 @@ describe('routes: poems', () => {
 
   test('GET a non-existing poem /api/v1/poems/:id', async () => {
     const response = await request(server).get('/api/v1/poems/999');
-    expect(response.status).toEqual(404);
+    expect(response.status).toEqual(CODE.NOT_FOUND);
     expect(response.type).toEqual('application/json');
     expect(response.body.message).toEqual('The poem does not exist.');
   });
@@ -58,7 +60,7 @@ describe('routes: poems', () => {
     const response = await request(server)
       .post('/api/v1/poems')
       .send({name: 'Test' + uuid.v1(), author: 'Test Author', votes: 6});
-    expect(response.status).toEqual(201);
+    expect(response.status).toEqual(CODE.CREATED);
     expect(response.type).toEqual('application/json');
     expect(Object.keys(response.body.data[0]).sort()).toEqual(
       ['id', 'name', 'author', 'votes', 'created_at', 'updated_at'].sort(),
@@ -67,9 +69,8 @@ describe('routes: poems', () => {
 
   test('POST nothing via /api/v1/poems', async () => {
     const response = await request(server).post('/api/v1/poems');
-    expect(response.status).toEqual(400);
+    expect(response.status).toEqual(CODE.BAD_REQUEST);
     expect(response.type).toEqual('application/json');
-    debug(response.body.data);
     expect(response.body.message).toEqual(
       'insert into "poems" default values returning * - null value in column "name" violates not-null constraint',
     );
@@ -79,9 +80,8 @@ describe('routes: poems', () => {
     const response = await request(server)
       .post('/api/v1/poems')
       .send({name: 'Test' + uuid.v1()});
-    expect(response.status).toEqual(400);
+    expect(response.status).toEqual(CODE.BAD_REQUEST);
     expect(response.type).toEqual('application/json');
-    debug(response.body.data);
     expect(response.body.message).toEqual(
       'insert into "poems" ("name") values ($1) returning * - null value in column "author" violates not-null constraint',
     );
@@ -91,7 +91,7 @@ describe('routes: poems', () => {
     const response = await request(server)
       .patch('/api/v1/poems/1')
       .send({name: 'Test' + uuid.v1()});
-    expect(response.status).toEqual(202);
+    expect(response.status).toEqual(CODE.ACCEPTED);
     expect(response.type).toEqual('application/json');
     expect(Object.keys(response.body.data[0]).sort()).toEqual(
       ['id', 'name', 'author', 'votes', 'created_at', 'updated_at'].sort(),
@@ -102,14 +102,14 @@ describe('routes: poems', () => {
     const response = await request(server)
       .patch('/api/v1/poems/999')
       .send({name: 'Test' + uuid.v1()});
-    expect(response.status).toEqual(404);
+    expect(response.status).toEqual(CODE.NOT_FOUND);
     expect(response.type).toEqual('application/json');
     expect(response.body.message).toEqual('The poem does not exist.');
   });
 
   test('DELETE a poem via /api/v1/poems/:id', async () => {
     const response = await request(server).delete('/api/v1/poems/1');
-    expect(response.status).toEqual(202);
+    expect(response.status).toEqual(CODE.ACCEPTED);
     expect(response.type).toEqual('application/json');
     expect(response.body.message).toEqual(
       'The poem has been deleted successfully.',
@@ -118,7 +118,7 @@ describe('routes: poems', () => {
 
   test('DELETE a non-existing poem via /api/v1/poems/:id', async () => {
     const response = await request(server).delete('/api/v1/poems/999');
-    expect(response.status).toEqual(404);
+    expect(response.status).toEqual(CODE.NOT_FOUND);
     expect(response.type).toEqual('application/json');
     expect(response.body.message).toEqual('The poem does not exist.');
   });
