e617ec5e127062ae4f08424746f864f169693091
Gabriel Wu <qqbbnease1004@126.com>
2019-04-06 09:34:03 +0800

    feat: upgrade to now v2

--- deploy/now.sh
+++ deploy/now.sh
@@ -2,6 +2,6 @@
 
 export NOW_SUBDOMAIN=koa-api-server
 
-NOW_DEPLOY_ID=$( now --token $NOW_TOKEN )
-now alias $NOW_DEPLOY_ID --token $NOW_TOKEN
+now --token $NOW_TOKEN
+now --target production --token $NOW_TOKEN
 now rm $NOW_SUBDOMAIN --safe --yes --token $NOW_TOKEN
